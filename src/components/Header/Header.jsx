3ea4c0d5b7b7e6c2095c55ff03e2216ee641c9cf
Julio Meza <juliomeza@gmail.com>
2025-08-14 13:14:30 -0400

    Listo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import styles from './Header.module.css';
-import logo from '../../assets/Logo.png';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className="container">
         <div className={styles.headerContent}>
-          <a href="#hero" className={styles.logoLink}>
-            <img src={logo} alt="Mind Exploration Logo" className={styles.logo} />
-            <span className={styles.logoText}>MIND EXPLORATION</span>
-          </a>
+          <div></div>
           
           <nav className={styles.nav}>
             <a href="#about" className={styles.navLink}>About</a>
